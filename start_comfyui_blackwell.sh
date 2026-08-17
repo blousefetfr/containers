c980f9cabb889de1733acb7de511a6bedf013e61
blousefetfr <131304869+blousefetfr@users.noreply.github.com>
2026-08-17 18:34:41 +0200

    Update start_comfyui_blackwell.sh

--- start_comfyui_blackwell.sh
+++ start_comfyui_blackwell.sh
@@ -79,9 +79,6 @@ if [ ! -d "$COMFYUI_DIR" ] || [ ! -d "$VENV_DIR" ]; then
     cd $COMFYUI_DIR
     echo "Installing and Compiling SageAttention2 and 3"
     cd /workspace/
-    git clone https://github.com/thu-ml/SageAttention.git
-    cd SageAttention 
-    EXT_PARALLEL=4 NVCC_APPEND_FLAGS="--threads 8" MAX_JOBS=32 python setup.py install
     cd sageattention3_blackwell
     EXT_PARALLEL=4 NVCC_APPEND_FLAGS="--threads 8" MAX_JOBS=32 python setup.py install
     echo "SageAttention2 and 3 installed"
